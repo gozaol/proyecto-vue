c690b2d842cddd47013e6b582c14a1202e680d74
antonio <gozaol@gmail.com>
2024-09-23 13:05:26 +0200

    contador carrito

---
+++ src/components/Carrito.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Mierda Vue</h1>
+    <p><img src="/src/components/imagenes/Screenshot_1.jpg" alt="" >Cesta <span >{{ contador }}</span> Productos</p>
+  
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+</template>
+  
+  <script  setup>
+  import {ref} from 'vue';
+  const contador = ref(0);
+  
+  const incrementar = ()=>{
+    contador.value++;
+  }
+  
+  const decrementar = ()=>{
+    if (contador.value > 0)
+    contador.value--;
+    else
+      contador.value=0;
+    
+  }
+  
+  </script>
+  <style scoped>
+  img{
+    width: 50px;
+  }
+  p{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  span{
+    color:red;
+    font-weight: bold;
+  }
+  </style>
